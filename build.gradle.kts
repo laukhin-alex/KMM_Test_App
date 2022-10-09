04d3e642ed53719dd75f26d3939bfa9b58ed33d0
laukhin-alex <alex.laukhin@yandex.ru>
2022-10-09 17:32:52 +0400

    dependences were added

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
     id("com.android.library").version("7.3.0").apply(false)
     kotlin("android").version("1.7.20").apply(false)
     kotlin("multiplatform").version("1.7.20").apply(false)
+    kotlin("plugin.serialization").version("1.7.20").apply(false)
 }
+
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    kotlin("plugin.serialization")
 }
 
 kotlin {
@@ -17,13 +18,28 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting
+        val ktorVersion = "2.0.1"
+        val commonMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-logging:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-        val androidMain by getting
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+            }
+        }
         val androidTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
@@ -33,6 +49,9 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+            }
         }
         val iosX64Test by getting
         val iosArm64Test by getting
