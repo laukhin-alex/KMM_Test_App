a00d2652a44a2d7e3392c114cc0fb89dc15e60f1
laukhin-alex <alex.laukhin@yandex.ru>
2022-10-09 21:11:10 +0400

    initial another API commit

--- shared/src/iosMain/kotlin/com/laukhin/kmm_test_app/Platform.kt
+++ shared/src/iosMain/kotlin/com/laukhin/kmm_test_app/Platform.kt
@@ -6,5 +6,4 @@ class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
-
 actual fun getPlatform(): Platform = IOSPlatform()
